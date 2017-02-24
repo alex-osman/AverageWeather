5bc098ed2f98497fde209260d54f858aaac90828
Alex <alexosman@live.com>
2017-02-24 16:11:10 -0500

    Added loading indicator

--- public/js/controllers/phevCtrl.js
+++ public/js/controllers/phevCtrl.js
@@ -1,12 +1,16 @@
 angular
   .module("ClimateChange")
   .controller("phevCtrl", ["$scope", "$http", function($scope, $http) {
-
     $scope.generate = function() {
+      $scope.loading = "Loading...";
       $http.get('/api/' + $scope.airport)
       .success(function(data) {
         console.log(data);
+        $scope.loading = "";
         $scope.temps = data;
+      })
+      .error(function(err) {
+        $scope.loading = "Something went wrong"
       })
     }
   }])
